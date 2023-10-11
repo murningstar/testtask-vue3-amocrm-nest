174e7489e29952e89c56429329d3d0c5eaa82497
menyadeniskostin <menyadeniskostin@gmail.com>
2023-10-11 04:54:28 +0300

    +leads, companies, contacts Controllers&Services

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { OauthClientServerController } from './oauth-client-server/oauth-client-server.controller';
 import { OauthClientServerService } from './oauth-client-server/oauth-client-server.service';
+import { LeadsController } from './leads/leads.controller';
+import { LeadsService } from './leads/leads.service';
+import { ContactsController } from './contacts/contacts.controller';
+import { ContactsService } from './contacts/contacts.service';
+import { CompaniesController } from './companies/companies.controller';
+import { CompaniesService } from './companies/companies.service';
 
 @Module({
   imports: [HttpModule, ConfigModule.forRoot(), CacheModule.register()],
-  controllers: [AppController, OauthClientServerController],
-  providers: [AppService, OauthClientServerService],
+  controllers: [AppController, OauthClientServerController, LeadsController, ContactsController, CompaniesController],
+  providers: [AppService, OauthClientServerService, LeadsService, ContactsService, CompaniesService],
 })
 export class AppModule {}
